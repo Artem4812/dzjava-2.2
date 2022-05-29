2ea2c033bffd04e94e80d92a8d389e140bb0e962
Artem4812 <artem.malchenko.1995@mail.ru>
2022-05-29 20:44:47 +0400

    dz 2.2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int replenishment = 2000;
+        int replenishment = 900 ;
         int bonus = replenishment / 100;
-        int check = balance + replenishment + bonus;
         if  (replenishment < 1000) {
             bonus = 0;
         }
+        int check = balance + replenishment + bonus;
         System.out.println("баланс =" + check);
     }
 }
