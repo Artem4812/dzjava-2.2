cb760dc816196477103b15f2ae77e497d34dd09c
Artem4812 <artem.malchenko.1995@mail.ru>
2022-05-30 10:57:51 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int replenishment = 900 ;
+        int replenishment = 900;
         int bonus = replenishment / 100;
-        if  (replenishment < 1000) {
+        if (replenishment < 1000) {
             bonus = 0;
         }
         int check = balance + replenishment + bonus;
         System.out.println("баланс =" + check);
     }
 }
+
